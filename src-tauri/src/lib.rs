55be38051f6fe5a52fee50d219449db318e71728
Mohammad Mansour <abdultawab940@gmail.com>
2024-07-11 22:13:43 +0300

    build: added another required configuration for Android

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,8 +7,8 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import path from "node:path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { internalIpV4 } from "internal-ip";
+
+const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -14,12 +17,19 @@ export default defineConfig(async () => ({
 	server: {
 		port: 1420,
 		strictPort: true,
+		host: mobile ? "0.0.0.0" : false,
+		hmr: mobile
+			? {
+					protocol: "ws",
+					host: await internalIpV4(),
+					port: 1421,
+				}
+			: undefined,
 		watch: {
 			// 3. tell vite to ignore watching `src-tauri`
 			ignored: ["**/src-tauri/**"],
 		},
 	},
-
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
